77fe61d76d106c9bc8a68d9cb15017bb863b7203
Robert Wiltshire <astrotutor9@gmail.com>
2025-04-19 12:24:18 +0100

    Added provisional waiting time for SD Card write after exposure

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from time import sleep
 
 relay = Pin(15, Pin.OUT)
 
-relay.value(1)
-sleep(0.25)
-relay.value(0)
+exposure_length_seconds = 5
+number_of_exposures = 4
+
+# This needs to be verified on camera
+# Length may change in relation to exposure length
+sdCard_write_time = 2
+
+def remote_release():
+    relay.value(1)
+    sleep(0.05)
+    relay.value(0)
+
+# Start exposure wait exposure length
+# Stop exposure wait for image to write to memory
+for exposures in range(number_of_exposures):
+    remote_release()               # exposure start
+    sleep(exposure_length_seconds)
+    remote_release()               # exposure stop
+    sleep(sdCard_write_time)
